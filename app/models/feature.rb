2fcbea0b7979ebd3d4241faf162b56fb549568d6
Hugh Sherman  Donkin <hsdonkin@gmail.com>
2019-08-23 11:55:28 -0700

    create campsites scope for Feature model

--- app/models/feature.rb
+++ app/models/feature.rb
@@ -1,2 +1,5 @@
 class Feature < ApplicationRecord
+
+  scope :campsites, -> {where("feature_type='Campsite'")}
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -15,5 +15,4 @@ Rails.application.routes.draw do
     end
   end
 
-  # resources :features
 end
