bf17f8299d14c69d25f2284e363429e9688b5928
Hugh Sherman  Donkin <hsdonkin@gmail.com>
2019-08-23 11:47:05 -0700

    implement custom routes in api namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   concern :api_base do
     resources :features
+    get "/", to: "features#index"
+    get "/random", to: "features#random"
+    get "/campsites", to: "features#campsites"
   end
 
 
@@ -11,8 +15,5 @@ Rails.application.routes.draw do
     end
   end
 
-  # get "/", to: "features#index"
-  # get "/random", to: "features#random"
-  # get "/campsites", to: "features#campsites"
   # resources :features
 end
