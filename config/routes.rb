90e4e2ea998fa13bbba7e7ed31eb8192341f008a
Hugh Sherman  Donkin <hsdonkin@gmail.com>
2019-08-23 11:46:20 -0700

    implement namespace for api routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  concern :api_base do
+    resources :features
+  end
+
+
+  namespace :api do
+    namespace :v1 do
+      concerns :api_base
+    end
+  end
+
+  # get "/", to: "features#index"
+  # get "/random", to: "features#random"
+  # get "/campsites", to: "features#campsites"
+  # resources :features
 end
